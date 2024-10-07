db91fdcacdade07b0c8f0f8ce8cfc5538405f2a5
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-10-07 10:13:15 +0400

    fix ( route ts )

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,39 +1,32 @@
 // // // /* eslint-disable @typescript-eslint/no-unused-vars */
 // import { convertCSVtoJSON } from "@/utils/parseData";
 
-// import { NextResponse } from "next/server";
-// import fs from "fs";
+import { NextResponse } from "next/server";
+
 // import prisma from "@/lib/prisma";
 
-// export async function GET() {
-//   const jsonFilePath = await convertCSVtoJSON();
-
-//   async function main() {
-//     if (!jsonFilePath) return;
-
-//     const fileContent = fs.readFileSync(jsonFilePath, "utf-8");
-//     const companies = JSON.parse(fileContent);
-//     console.log(companies.length);
-
-//     //   console.log(companies.length, "companies");
-
-//     // await prisma.company.deleteMany({});
-
-//     await prisma.company.createMany({
-//       data: companies,
-//       skipDuplicates: true,
-//     });
-//   }
-
-//   main()
-//     .catch((e) => {
-//       console.error("Error in main function:", e);
-//       process.exit(1);
-//     })
-//     .finally(async () => {
-//       const response = await prisma.$disconnect();
-//       console.log(response);
-//     });
-
-//   return NextResponse.json({ message: "Success" });
-// }
+export async function GET() {
+  //   const jsonFilePath = await convertCSVtoJSON();
+  //   async function main() {
+  //     if (!jsonFilePath) return;
+  //     const fileContent = fs.readFileSync(jsonFilePath, "utf-8");
+  //     const companies = JSON.parse(fileContent);
+  //     console.log(companies.length);
+  //     //   console.log(companies.length, "companies");
+  //     // await prisma.company.deleteMany({});
+  //     await prisma.company.createMany({
+  //       data: companies,
+  //       skipDuplicates: true,
+  //     });
+  //   }
+  //   main()
+  //     .catch((e) => {
+  //       console.error("Error in main function:", e);
+  //       process.exit(1);
+  //     })
+  //     .finally(async () => {
+  //       const response = await prisma.$disconnect();
+  //       console.log(response);
+  //     });
+  return NextResponse.json({ message: "Success" });
+}
